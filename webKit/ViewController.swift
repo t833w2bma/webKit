56631bc451a1aac1e56ff2b65dff836764a68778
ginzo <ginzo@ginzo-no-mac-mini.local>
2018-07-31 08:03:18 +0900

    urlをリテラルに指定

--- webKit/ViewController.swift
+++ webKit/ViewController.swift
@@ -15,12 +15,12 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
         let aplUrl = "https://www.apple.com/jp/"
         label.text = aplUrl
         let applecom = URL(string: aplUrl)!
         webkit.load(URLRequest(url: applecom))
-        super.viewDidLoad()
       
         
     }
